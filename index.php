6a8e01176a377206eef0ee7772d3868841e714d7
nileshsurya1994 <114112246+nileshsurya1994@users.noreply.github.com>
2024-02-05 13:58:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 /**
  * Tells WordPress to load the WordPress theme and output it.
  * 
- * Hi every one This is Nilesh !!
+ * Hi every one This is Sachin !!
  * @var bool
  */
 define( 'WP_USE_THEMES', true );
